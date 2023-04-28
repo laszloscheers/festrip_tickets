4a54c6620b78627f7a5b7c099a906d8c17123204
Laszlo-Scheers <laszloscheers@gmail.com>
2023-04-28 11:20:32 +0200

    mailer_sender fixed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Festrip Tickets Platform'
+  config.mailer_sender = '"Festrip Tickets" <festriptickets@gmail.com>'
   config.navigational_formats = ['*/*', :html, :turbo_stream]
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
